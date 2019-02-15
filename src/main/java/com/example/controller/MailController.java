49ca65591eb1968e0eddea78926d527f66875ded
Rajkumar Katanor <rkatanor@valuelabs.com>
2019-02-14 18:11:25 -0800

    just changed name of msg

--- src/main/java/com/example/controller/MailController.java
+++ src/main/java/com/example/controller/MailController.java
@@ -25,6 +25,6 @@ public class MailController {
 	}
 	@RequestMapping("/hello")
 	public String hello() {
-		return "hello heroku ...nice to meet you again again...lilly";
+		return "nice to meet you again again...lilly";
 	}
 }
